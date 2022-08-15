af12ee789cb77613f0dbe8836b3b19942607629c
Thomas Sauer <102015688+tbsauer@users.noreply.github.com>
2022-08-15 13:43:12 -0700

    Add files via upload

---
+++ vue/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ]
+})
+
+module.exports = {
+  devServer:{
+    proxy: "https://localhost:7222/"
+  }
+}
