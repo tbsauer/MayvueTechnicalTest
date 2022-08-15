480868cdd9461fa1cf83804070a01fbc84b62531
Thomas Sauer <102015688+tbsauer@users.noreply.github.com>
2022-08-15 13:38:42 -0700

    Add files via upload

---
+++ interfaces/IMotionPictureService.cs
@@ -0,0 +1,13 @@
+﻿using MotionPicturesCore.Models;
+
+namespace MotionPicturesCore.Interfaces
+{
+    public interface IMotionPictureService
+    {
+        public Task<IEnumerable<MotionPicture>> GetMotionPictures();
+        public Task<MotionPicture> GetMotionPicture(int id);
+        public Task<MotionPicture> CreateMotionPicture(MotionPictureAddRequest movie);
+        public Task UpdateMotionPicture(int id, MotionPictureUpdateRequest movie);
+        public Task DeleteMotionPicture(int id);
+    }
+}
